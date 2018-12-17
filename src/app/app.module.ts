1d8812c76be9f2ddd0de430192a3a62f4f00ed62
alex.paclin <alex.paclin@gmail.com>
2018-12-17 15:57:23 +0500

    add @angular/elements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HelloComponent } from './hello/hello.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
+  entryComponents: [
+    HelloComponent
+  ],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
